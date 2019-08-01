94012922e57c4f213b0c7326947c2cbb6511fca5
Brian Kimathi <kimbrian400@gmail.com>
2019-08-01 11:13:55 +0300

    UI
    Signed-off-by: Brian Kimathi <kimbrian400@gmail.com>

--- app/src/main/java/com/kimbrian/phoneauthentication/MainActivity.java
+++ app/src/main/java/com/kimbrian/phoneauthentication/MainActivity.java
@@ -1,14 +1,51 @@
 package com.kimbrian.phoneauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText editTextCountryCode, editTextPhone;
+    AppCompatButton buttonContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextCountryCode = findViewById(R.id.editTextCountryCode);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        buttonContinue = findViewById(R.id.buttonContinue);
+
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = editTextCountryCode.getText().toString().trim();
+                String number = editTextPhone.getText().toString().trim();
+
+                if (number.isEmpty() || number.length() < 10) {
+                    editTextPhone.setError("Valid number is required");
+                    editTextPhone.requestFocus();
+                    return;
+                }
+
+                String phoneNumber = code + number;
+
+                Intent intent = new Intent(MainActivity.this, VerificationCode.class);
+                intent.putExtra("phoneNumber", phoneNumber);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/kimbrian/phoneauthentication/VerificationCode.java
@@ -0,0 +1,14 @@
+package com.kimbrian.phoneauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerificationCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification_code);
+    }
+}
